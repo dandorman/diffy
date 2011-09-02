0763b0aa6b2230870527ebd88c46c8b50d2d827e
Dan Dorman <dan.dorman@gmail.com>
2011-09-02 15:46:52 -0600

    Refactor API.

--- diffy.py
+++ diffy.py
@@ -7,24 +7,23 @@ init()
 
 class Diffy(object):
 
-  def __init__(self, a, b):
-    self.a = a
-    self.b = b
+  def __init__(self):
+    self.differ = dmp_module.diff_match_patch()
+    self.diff_output = []
 
-  def diff(self):
-    differ = dmp_module.diff_match_patch()
-    diffs = differ.diff_main(self.a, self.b)
+  def diff(self, a, b):
+    diffs = self.differ.diff_main(a, b)
 
     top_line, bottom_line = "", ""
     for position, (action, text) in enumerate(diffs):
-      if action == differ.DIFF_EQUAL:
+      if action == self.differ.DIFF_EQUAL:
         top_line += text
         bottom_line += text
 
-      elif action == differ.DIFF_DELETE:
+      elif action == self.differ.DIFF_DELETE:
         next_action, next_text = position + 1 < len(diffs) and diffs[position + 1] or (None, None)
 
-        if next_action != differ.DIFF_INSERT:
+        if next_action != self.differ.DIFF_INSERT:
           top_line += self.engorge(text)
           bottom_line += " " * len(text)
         elif not next_action:
@@ -32,16 +31,17 @@ class Diffy(object):
         else:
           top_line += self.jaundice(text.center(max(len(next_text), len(text))))
 
-      elif action == differ.DIFF_INSERT:
+      elif action == self.differ.DIFF_INSERT:
         previous_action, previous_text = position - 1 >= 0 and diffs[position - 1] or (None, None)
 
-        if previous_action == differ.DIFF_EQUAL:
+        if previous_action == self.differ.DIFF_EQUAL:
           bottom_line += self.putrefy(text)
           top_line += " " * len(text)
         else:
           bottom_line += self.jaundice(text.center(max(len(previous_text), len(text))))
 
-    return [top_line, bottom_line]
+    self.diff_output = [top_line, bottom_line]
+    return self
 
   def colorize(self, color, text):
     return Style.BRIGHT + color + text + Style.RESET_ALL
@@ -55,14 +55,14 @@ class Diffy(object):
   def putrefy(self, text):
     return self.colorize(Fore.GREEN, text)
 
-  def output(self):
-    return "\n".join(self.diff())
+  def display(self):
+    print "\n".join(self.diff_output)
 
 if __name__ == "__main__":
   if len(sys.argv) == 3:
     a = sys.argv[-2]
     b = sys.argv[-1]
 
-    print Diffy(a, b).output()
+    Diffy().diff(a, b).display()
   else:
     print "USAGE: diffy [a string] [another string]"
