ed61cc7f695a6a192cc4df0e0b352f26306bbfa4
Dan Dorman <dan.dorman@gmail.com>
2011-09-02 15:05:41 -0600

    Refactor code to make it a little less nasty.

--- diffy.py
+++ diffy.py
@@ -5,38 +5,59 @@ from colorama import init, Fore, Style
 
 init()
 
-a = sys.argv[-2]
-b = sys.argv[-1]
+class Diffy(object):
 
-differ = dmp_module.diff_match_patch()
-diffs = differ.diff_main(a, b)
-differ.diff_cleanupSemantic(diffs)
+  def __init__(self, a, b):
+    self.a = a
+    self.b = b
 
-top_line, bottom_line = "", ""
-i = 0
-while i < len(diffs):
-  pair = diffs[i]
-  if pair[0] == differ.DIFF_EQUAL:
-    top_line += Style.RESET_ALL + pair[1]
-    bottom_line += Style.RESET_ALL + pair[1]
+  def diff(self):
+    differ = dmp_module.diff_match_patch()
+    diffs = differ.diff_main(self.a, self.b)
+    differ.diff_cleanupSemantic(diffs)
 
-  elif pair[0] == differ.DIFF_DELETE:
-    if i + 1 < len(diffs) and diffs[i + 1][0] != differ.DIFF_INSERT:
-      top_line += Style.BRIGHT + Fore.RED + pair[1]
-      bottom_line += Style.RESET_ALL + (" " * len(pair[1]))
-    elif i + 1 == len(diffs):
-      top_line += Style.BRIGHT + Fore.RED + pair[1]
-    else:
-      top_line += Style.BRIGHT + Fore.YELLOW + pair[1]
+    top_line, bottom_line = "", ""
+    for position, (action, text) in enumerate(diffs):
 
-  elif pair[0] == differ.DIFF_INSERT:
-    if i - 1 < 0 or diffs[i - 1][0] == differ.DIFF_EQUAL:
-      bottom_line += Style.BRIGHT + Fore.GREEN + pair[1]
-      top_line += Style.RESET_ALL + (" " * len(pair[1]))
-    else:
-      bottom_line += Style.BRIGHT + Fore.YELLOW + pair[1]
+      if action == differ.DIFF_EQUAL:
+        top_line += text
+        bottom_line += text
 
-  i += 1
+      elif action == differ.DIFF_DELETE:
+        if position + 1 < len(diffs) and diffs[position + 1][0] != differ.DIFF_INSERT:
+          top_line += self.engorge(text)
+          bottom_line += " " * len(text)
+        elif position + 1 == len(diffs):
+          top_line += self.engorge(text)
+        else:
+          top_line += self.jaundice(text)
 
-print top_line
-print bottom_line
+      elif action == differ.DIFF_INSERT:
+        if position - 1 < 0 or diffs[position - 1][0] == differ.DIFF_EQUAL:
+          bottom_line += self.putrefy(text)
+          top_line += " " * len(text)
+        else:
+          bottom_line += self.jaundice(text)
+
+    return [top_line, bottom_line]
+
+  def colorize(self, color, text):
+    return Style.BRIGHT + color + text + Style.RESET_ALL
+
+  def engorge(self, text):
+    return self.colorize(Fore.RED, text)
+
+  def jaundice(self, text):
+    return self.colorize(Fore.YELLOW, text)
+
+  def putrefy(self, text):
+    return self.colorize(Fore.GREEN, text)
+
+  def output(self):
+    return "\n".join(self.diff())
+
+if __name__ == "__main__":
+  a = sys.argv[-2]
+  b = sys.argv[-1]
+
+  print Diffy(a, b).output()
